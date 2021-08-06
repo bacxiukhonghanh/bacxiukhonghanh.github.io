4b587a2415af5c7fc10c5494fcbbb2c163d4933e
bacxiukhonghanh <markrobinsonsecurity@gmail.com>
2021-08-06 19:15:43 +0700

    ASCII Art Img: fix image path

--- art/src/index.js
+++ art/src/index.js
@@ -34,7 +34,7 @@ window.onload = function() {
             // fileInput.style.display = 'none';
             drawAscii(grayScales, width);
         }
-        image.src = "/art/img/" + filename + ".jpg";
+        image.src = "./img/" + filename + ".jpg";
     }
     else {
         document.title = "Error";
